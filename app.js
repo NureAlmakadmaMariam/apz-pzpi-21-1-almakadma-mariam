633ee19a03b88ef97e82892e84feb8500840135a
malma <mariam.almakadma@nure.ua>
2024-04-24 15:54:08 +0200

    test and run server

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Сервер Node.js з базою даних PostgreSQL працює успішно!');
+});
+
+const port =  3000;
+
+app.listen(port, () => {
+    console.log(`Сервер запущено на порті ${port}`);
+});
