9f261e5ce0c5a39b07d6600d31bbe31cedc686ea
malma <mariam.almakadma@nure.ua>
2024-05-09 09:42:12 +0200

    create achievements  + user gets points

--- controllers/achievementController.js
+++ controllers/achievementController.js
@@ -1,6 +1,38 @@
 // achievementController.js
-
 const Achievement = require('../models/achievementModel');
+const User = require('../models/userModel');
+
+exports.createAchievement = async (req, res) => {
+    try {
+        const { title, description, points_awarded, user_id } = req.body;
+        let { date_achieved } = req.body;
+
+        if (!date_achieved) {
+            date_achieved = new Date();
+        }
+
+        const achievement = await Achievement.create({
+            title,
+            description,
+            points_awarded,
+            date_achieved,
+            user_id,
+        });
+
+        if (points_awarded) {
+            const user = await User.findByPk(user_id);
+            if (user) {
+                user.points = user.points ? user.points + points_awarded : points_awarded;
+                await user.save();
+            }
+        }
+
+        return res.status(201).json({ message: 'Achievement created successfully', achievement });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
 
 exports.deleteAchievementById = async (req, res) => {
     try {
--- routes/achievementRouter.js
+++ routes/achievementRouter.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const achievementController = require('../controllers/achievementController');
 
-
+router.post('/', achievementController.createAchievement);
 router.delete('/:achievementId', achievementController.deleteAchievementById);
 
 module.exports = router;
