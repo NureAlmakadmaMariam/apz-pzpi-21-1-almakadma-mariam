161e54d0e84935b9a5b29eb01b247a5ba0df5cf8
malma <mariam.almakadma@nure.ua>
2024-05-21 16:45:41 +0200

    видалення користувача

---
+++ Task4/apz-pzpi-21-1-almakadma-mariam-task4/src/components/DeleteUserButton.tsx
@@ -0,0 +1,32 @@
+// src/components/DeleteUserButton.tsx
+import React from 'react';
+import { User } from '../interfaces/User';
+import { FormattedMessage } from 'react-intl';
+import styles from '../styles/UserList.module.css';
+import { deleteUser } from '../features/users'; // Імпортуємо функцію deleteUser
+
+interface DeleteUserButtonProps {
+    user: User;
+    onDelete: () => void;
+}
+
+const DeleteUserButton: React.FC<DeleteUserButtonProps> = ({ user, onDelete }) => {
+    const handleDelete = async () => {
+        try {
+            await deleteUser(user.user_id); // Викликаємо функцію для видалення користувача
+            onDelete(); // Викликаємо колбек-функцію onDelete для оновлення списку користувачів
+        } catch (error) {
+            console.error('Failed to delete user:', error);
+            // Обробка помилки видалення користувача
+        }
+    };
+
+    return (
+        <button onClick={handleDelete} className={styles.deleteButton}>
+            <FormattedMessage id="user.delete" />
+        </button>
+    );
+};
+
+export default DeleteUserButton;
+
