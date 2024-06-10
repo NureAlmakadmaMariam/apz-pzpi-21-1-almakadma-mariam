3e36361d984f81769e5aae079debf0866d85df52
malma <mariam.almakadma@nure.ua>
2024-06-10 20:00:57 +0200

    file

--- Task5/apz-pzpi-21-1-almakadma-mariam-task5/app/src/main/java/com/example/performmentor/network/RetrofitInstance.kt
+++ Task5/apz-pzpi-21-1-almakadma-mariam-task5/app/src/main/java/com/example/performmentor/network/RetrofitInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://192.168.67.169:3500/"
+    private const val BASE_URL = ""
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
